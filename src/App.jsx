ea3aac0bc1f025f8131bf4ee02445b9885c298ba
vanshika gupta <vanshikagupta503@gmail.com>
2026-03-23 21:31:49 +0530

    Day1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from './Components/Profile'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My first React Project</h1>
+      <Profile/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Vanshika Gupta</h2>
+      <p>Frontened Developer</p>
+      <p>Learning React Step by Step</p>
+    </div>
+  )
+}
+
+export default Profile
+
+
